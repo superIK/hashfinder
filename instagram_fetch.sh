28b2f10a25c3ecad6c21b6bad4fbd11e4b6f8b88
Ibtissam Karouach <ik@pysj.org>
2014-11-03 21:32:42 +0100

    modified:   instagram_fetch.sh

--- instagram_fetch.sh
+++ instagram_fetch.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
+echo "Starting to fetch JSON-files from Instagram"
 for run in {1..60}
 do
  curl -sL 'https://api.instagram.com/v1/tags/snow/media/recent?access_token=1549208576.dd99346.d7f8f2f6b0964562ad7330b662bdfdbf' > hash.txt
  content=`cat hash.txt`
- curl -XPOST http://127.0.0.1:10018/riak/instagram3 -H 'Content-Type: text/plain' \  -d "$content" -s
+ curl -XPOST http://127.0.0.1:10018/riak/instagram -H 'Content-Type: text/plain' \  -d "$content" -s
  sleep 1
+ echo "Fetched one new JSON from Instagram........"
 done
+echo "Done fetching data....."
+echo ""
+echo "To list all Keys in the bucket use this comand:"
+echo "curl 'localhost:10018/buckets/instagram/keys?keys=true'"
+echo ""
+echo "To see data from a key use the following command:"
+echo "curl -v http://127.0.0.1:10018/riak/instagram/KEY"
+echo ""
